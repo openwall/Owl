42ea910c9d680c70620d67130f9da0eb5e3c44de
kad <kad>
2000-08-23 15:17:53 +0000

    new ""
    CVS-ID: packages/less/lesspipe.sh 1.3

--- packages/less/lesspipe.sh
+++ packages/less/lesspipe.sh
@@ -10,18 +10,18 @@ lesspipe() {
   *.tar.gz) tar tzvvf "$1" 2>/dev/null ;;
   *.tar.bz2) bzip2 -dc "$1" | tar tvvf - 2>/dev/null ;;
   *.tar.Z) tar tzvvf "$1" 2>/dev/null ;;
-  *.tar.z) tar tzvvf $1 2>/dev/null ;;
+  *.tar.z) tar tzvvf "$1" 2>/dev/null ;;
   *.Z) gzip -dc "$1"  2>/dev/null ;; # View compressed files correctly
   *.z) gzip -dc "$1"  2>/dev/null ;;
   *.gz) gzip -dc "$1"  2>/dev/null ;;
   *.bz2) bzip2 -dc "$1"  2>/dev/null ;;
   *.zip) unzip -l "$1" 2>/dev/null ;;
   *.rpm) rpm -qpivl "$1" 2>/dev/null ;; # view contents of .rpm files
-  *.1|*.2|*.3|*.4|*.5|*.6|*.7|*.8|*.9|*.n|*.man) FILE=`file -L "$1"` ; # groff src
-    FILE=`echo $FILE | cut -d ' ' -f 2`
-    if [ "$FILE" = "troff" ]; then
-      groff -s -p -t -e -Tascii -mandoc $1
-    fi ;;
+  *.1|*.2|*.3|*.4|*.5|*.6|*.7|*.8|*.9|*.n|*.man) 
+   FILE=`file -L "$1" | grep ": troff or preprocessor input text"` ; # groff src
+   if [ -n "$FILE" ]; then
+     groff -s -p -t -e -Tascii -mandoc "$1"
+   fi ;;
   esac
 }
 
