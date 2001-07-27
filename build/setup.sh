64d4b6dac80a2bf236a60c3475da58d461057585
Solar Designer <solar@openwall.com>
2001-07-28 03:25:12 +0400

    Unset TMPDIR and TMP when ROOT != / as these may not be valid under the new root.
    CVS-ID: build/setup.sh 1.4

--- build/setup.sh
+++ build/setup.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# $Id: Owl/build/setup.sh,v 1.3 2001/06/20 21:02:14 solar Exp $
+# $Id: Owl/build/setup.sh,v 1.4 2001/07/27 23:25:12 solar Exp $
 
 . installworld.conf
 
@@ -11,4 +11,7 @@ fi
 umask $UMASK
 
 export PATH=/sbin:/usr/sbin:$PATH
+if [ $ROOT != / ]; then
+	export TMPDIR= TMP=
+fi
 chroot $ROOT /usr/sbin/setup
