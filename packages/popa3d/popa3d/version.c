da78ab05ac898dbeeb1d4a7f7e33741f5695440e
Solar Designer <solar@openwall.com>
2003-03-10 03:30:38 +0300

    0.6.2
    CVS-ID: packages/popa3d/popa3d/version.c 1.2

--- packages/popa3d/popa3d/version.c
+++ packages/popa3d/popa3d/version.c
@@ -2,6 +2,6 @@
  * popa3d version information.
  */
 
-char popa3d_version[] = "0.6.1";
-char popa3d_date[] = "$Date: 2003/03/02 03:38:17 $";
-char popa3d_id[] = "@(#) $Id: Owl/packages/popa3d/popa3d/version.c,v 1.1 2003/03/02 03:38:17 solar Exp $";
+char popa3d_version[] = "0.6.2";
+char popa3d_date[] = "$Date: 2003/03/10 00:30:38 $";
+char popa3d_id[] = "@(#) $Id: Owl/packages/popa3d/popa3d/version.c,v 1.2 2003/03/10 00:30:38 solar Exp $";
