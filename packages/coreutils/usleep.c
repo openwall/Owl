4631c71576dbdc183053d8fa333bd73b73cdfa60
Solar Designer <solar@openwall.com>
2005-05-08 01:48:26 +0400

    Added a public domain notice as approved by Dmitry on owl-devel.
    Re-formatted the source for another time, per the conventions used for
    our own code in Owl.
    
    Force base-10 numbers with strtoul().  The "support" of base 8 and 16
    delay values with this implementation of usleep(1) was hardly expected
    by the users.
    
    CVS-ID: packages/coreutils/usleep.c 1.3

--- packages/coreutils/usleep.c
+++ packages/coreutils/usleep.c
@@ -1,5 +1,11 @@
+/*
+ * Written by Dmitry V. Levin for ALT Linux and placed in the public domain.
+ * Further modifications by Solar Designer for Openwall GNU/*/Linux, still
+ * public domain.  There's absolutely no warranty.
+ */
+
 #ifndef _GNU_SOURCE
-#define _GNU_SOURCE 1
+#define _GNU_SOURCE
 #endif
 
 #include <stdio.h>
@@ -9,38 +15,34 @@
 #include <limits.h>
 #include <unistd.h>
 
-static void __attribute__ ((__noreturn__))
-usage (void)
+static void __attribute__ ((__noreturn__)) usage(void)
 {
-	fprintf (stderr, "usage: %s [microseconds]\n",
-		 program_invocation_short_name);
+	fprintf(stderr, "usage: %s [microseconds]\n",
+		program_invocation_short_name);
 
-	exit (EXIT_FAILURE);
+	exit(EXIT_FAILURE);
 }
 
-int
-main (int argc, const char **argv)
+int main(int argc, const char **argv)
 {
 	unsigned long delay = 1;
 
 	if (argc > 2)
-		usage ();
+		usage();
 
-	if (argc == 2)
-	{
-		char   *p = 0;
+	if (argc == 2) {
+		char *p = 0;
 
 		errno = 0;
-		delay = strtoul (argv[1], &p, 0);
-		if (!*argv[1] || *p || errno)
-		{
-			error (EXIT_SUCCESS, errno ? : EINVAL, "%s", argv[1]);
-			usage ();
+		delay = strtoul(argv[1], &p, 10);
+		if (!*argv[1] || *p || errno) {
+			error(EXIT_SUCCESS, errno ? : EINVAL, "%s", argv[1]);
+			usage();
 		}
 	}
 
-	if (usleep (delay))
-		error (EXIT_FAILURE, errno, "%s", argv[1]);
+	if (usleep(delay))
+		error(EXIT_FAILURE, errno, "%s", argv[1]);
 
 	return EXIT_SUCCESS;
 }
