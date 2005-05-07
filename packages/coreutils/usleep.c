3ec6fb7a3ffc52da4e9eee62797db2f18f6a9c55
Solar Designer <solar@openwall.com>
2005-05-08 02:26:06 +0400

    Oops, can't have "Openwall GNU/*/Linux" within a C comment.  Nasty.
    CVS-ID: packages/coreutils/usleep.c 1.5

--- packages/coreutils/usleep.c
+++ packages/coreutils/usleep.c
@@ -1,9 +1,10 @@
 /*
- * Written by Dmitry V. Levin for ALT Linux and placed in the public domain.
- * Further modifications by Solar Designer for Openwall GNU/*/Linux, still
- * public domain.  There's absolutely no warranty.
+ * Written by Dmitry V. Levin and placed in the public domain.
+ * Further modifications by Solar Designer, still public domain.
  *
- * $Id: Owl/packages/coreutils/usleep.c,v 1.4 2005/05/07 21:48:56 solar Exp $
+ * There's absolutely no warranty.
+ *
+ * $Id: Owl/packages/coreutils/usleep.c,v 1.5 2005/05/07 22:26:06 solar Exp $
  */
 
 #ifndef _GNU_SOURCE
