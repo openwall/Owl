e6eb259bc9b3f557e5861f377ee5f26771d6e47d
Dmitry V. Levin <ldv@altlinux.org>
2006-10-26 20:12:29 +0000

    Removed explicit loopback device specification.
    CVS-ID: packages/owl-cdrom/floppy-update.sh 1.5

--- packages/owl-cdrom/floppy-update.sh
+++ packages/owl-cdrom/floppy-update.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
-# $Owl: Owl/packages/owl-cdrom/Attic/floppy-update.sh,v 1.4 2006/10/26 20:10:02 ldv Exp $
+# $Owl: Owl/packages/owl-cdrom/Attic/floppy-update.sh,v 1.5 2006/10/26 20:12:29 ldv Exp $
 
 set -e
 
 dd if=/dev/zero of=floppy.image bs=36k count=80
 /sbin/mke2fs -m 0 -F floppy.image
 
-mount floppy.image /mnt/floppy -oloop=/dev/loop0
+mount floppy.image /mnt/floppy -oloop
 trap 'umount /mnt/floppy' EXIT HUP INT TERM
 
 mkdir -m 700 /mnt/floppy/boot
