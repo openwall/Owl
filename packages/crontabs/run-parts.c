3c9e8d8d20df53af63202cf1296965f51af863f6
Dmitry V. Levin <ldv@altlinux.org>
2006-12-26 15:26:46 +0000

    Obfuscated e-mail addresses. Made all local functions and variables static. Replaced homebrew error() with GNU error(3). Changed --version output from stderr to stdout. Changed --help output from stderr to stdout. Moved usage diagnostics to separate function. Fixed --umask value validation. Fixed regex error reporting. Reworked regex code to avoid duplication and memory leaks. Fixed standard descriptors in --report mode. Replaced single write(2) calls with write_loop() calls. Fixed errors handling in directory processing. Changed --lsbsysinit exception regex from "^[a-z0-9-].*dpkg-(old|dist|new|tmp)$" to "^[a-z0-9-].*rpm(old|new|orig)$". Changed default umask from 022 to 077. Added "-owl" suffix to version string.
    CVS-ID: packages/crontabs/run-parts.c 1.2

--- packages/crontabs/run-parts.c
+++ packages/crontabs/run-parts.c
@@ -1,9 +1,9 @@
 /* run-parts: run a bunch of scripts in a directory
  *
  * Debian run-parts program
- * Copyright (C) 1996 Jeff Noxon <jeff@router.patch.net>,
- * Copyright (C) 1996-1999 Guy Maor <maor@debian.org>
- * Copyright (C) 2002, 2003, 2004, 2005 Clint Adams <schizo@debian.org>
+ * Copyright (C) 1996 Jeff Noxon <jeff at router.patch.net>,
+ * Copyright (C) 1996-1999 Guy Maor <maor at debian.org>
+ * Copyright (C) 2002, 2003, 2004, 2005 Clint Adams <schizo at debian.org>
  *
  * This is free software; see the GNU General Public License version 2
  * or later for copying conditions.  There is NO warranty.
@@ -11,6 +11,10 @@
  * Based on run-parts.pl version 0.2, Copyright (C) 1994 Ian Jackson.
  *
  */
+
+#ifndef _GNU_SOURCE
+# define _GNU_SOURCE 1
+#endif
 
 #include <stdio.h>
 #include <stdarg.h>
@@ -21,172 +25,226 @@
 #include <sys/stat.h>
 #include <unistd.h>
 #include <fcntl.h>
-#ifdef HAVE_GETOPT_H
 #include <getopt.h>
-#endif /* HAVE_GETOPT_H */
 #include <string.h>
 #include <errno.h>
+#include <error.h>
 #include <ctype.h>
 #include <signal.h>
 #include <sys/time.h>
 #include <regex.h>
 
-int test_mode = 0;
-int list_mode = 0;
-int verbose_mode = 0;
-int report_mode = 0;
-int reverse_mode = 0;
-int exitstatus = 0;
-int lsbsysinit_mode = 0;
-int exit_on_error_mode = 0;
+#define PACKAGE_VERSION "2.17.4-owl"
 
-int argcount = 0, argsize = 0;
-char **args = 0;
+static int test_mode = 0;
+static int list_mode = 0;
+static int verbose_mode = 0;
+static int report_mode = 0;
+static int reverse_mode = 0;
+static int exitstatus = 0;
+static int lsbsysinit_mode = 0;
+static int exit_on_error_mode = 0;
 
-void error(char *format, ...)
+static int argcount = 0, argsize = 0;
+static const char **args = NULL;
+
+static void
+my_error_print_progname(void)
 {
-  va_list ap;
-
-  fprintf(stderr, "run-parts: ");
-
-  va_start(ap, format);
-  vfprintf(stderr, format, ap);
-  va_end(ap);
-
-  fprintf(stderr, "\n");
+  fprintf(stderr, "%s: ", program_invocation_short_name);
 }
 
-
-void version()
+static void __attribute__((noreturn))
+print_version()
 {
-  fprintf(stderr, "Debian run-parts program, version " PACKAGE_VERSION
-	  "\nCopyright (C) 1994 Ian Jackson, Copyright (C) 1996 Jeff Noxon.\n"
-	  "Copyright (C) 1996,1997,1998,1999 Guy Maor\n"
-	  "Copyright (C) 2002, 2003, 2004, 2005 Clint Adams\n"
-	  "This is free software; see the GNU General Public License version 2\n"
-	  "or later for copying conditions.  There is NO warranty.\n");
-  exit(0);
+  printf("Debian run-parts program, version " PACKAGE_VERSION
+	 "\nCopyright (C) 1994 Ian Jackson, Copyright (C) 1996 Jeff Noxon.\n"
+	 "Copyright (C) 1996,1997,1998,1999 Guy Maor\n"
+	 "Copyright (C) 2002, 2003, 2004, 2005 Clint Adams\n"
+	 "This is free software; see the GNU General Public License version 2\n"
+	 "or later for copying conditions.  There is NO warranty.\n");
+  exit(EXIT_SUCCESS);
 }
 
-
-void usage()
+static void __attribute__((noreturn))
+print_help(void)
 {
-  fprintf(stderr, "Usage: run-parts [OPTION]... DIRECTORY\n"
-	  "      --test          print script names which would run, but don't run them.\n"
-	  "      --list          print names of all valid files (can not be used with\n"
-	  "                      --test)\n"
-	  "  -v, --verbose       print script names before running them.\n"
-	  "      --report        print script names if they produce output.\n"
-	  "      --reverse       reverse execution order of scripts.\n"
-	  "      --exit-on-error exit as soon as a script returns with a non-zero exit\n"
-	  "                      code.\n"
-	  "      --lsbsysinit    validate filenames based on LSB sysinit specs.\n"
-	  "  -u, --umask=UMASK   sets umask to UMASK (octal), default is 022.\n"
-	  "  -a, --arg=ARGUMENT  pass ARGUMENT to scripts, use once for each argument.\n"
-	  "  -V, --version       output version information and exit.\n"
-	  "  -h, --help          display this help and exit.\n");
-  exit(0);
+  printf("Usage: %s [OPTION]... DIRECTORY\n"
+	 "      --test          print script names which would run, but don't run them.\n"
+	 "      --list          print names of all valid files (can not be used with\n"
+	 "                      --test)\n"
+	 "  -v, --verbose       print script names before running them.\n"
+	 "      --report        print script names if they produce output.\n"
+	 "      --reverse       reverse execution order of scripts.\n"
+	 "      --exit-on-error exit as soon as a script returns with a non-zero exit\n"
+	 "                      code.\n"
+	 "      --lsbsysinit    validate filenames based on LSB sysinit specs.\n"
+	 "  -u, --umask=UMASK   sets umask to UMASK (octal), default is 077.\n"
+	 "  -a, --arg=ARGUMENT  pass ARGUMENT to scripts, use once for each argument.\n"
+	 "  -V, --version       output version information and exit.\n"
+	 "  -h, --help          display this help and exit.\n",
+	 program_invocation_short_name);
+  exit(EXIT_SUCCESS);
 }
 
-
-/* The octal conversion in libc is not foolproof; it will take the 8 and 9
- * digits under some circumstances.  We'll just have to live with it.
- */
-void set_umask()
+static void __attribute__((noreturn, format(printf, 1, 2)))
+show_usage(const char *fmt, ...)
 {
-  int mask, result;
+  if (fmt) {
+    va_list arg;
 
-  result = sscanf(optarg, "%o", &mask);
-  if ((result != 1) || (mask > 07777) || (mask < 0)) {
-    error("bad umask value");
-    exit(1);
+    fprintf(stderr, "%s: ", program_invocation_short_name);
+    va_start(arg, fmt);
+    vfprintf(stderr, fmt, arg);
+    va_end(arg);
+    fputc('\n', stderr);
   }
+  fprintf(stderr, "Try `%s --help' for more information.\n",
+	  program_invocation_short_name);
+  exit(EXIT_FAILURE);
+}
 
-  umask(mask);
+static void
+set_umask(const char *str)
+{
+  char *p = NULL;
+  unsigned long n = strtoul(str, &p, 8);
+
+  if (!*str || !p || *p || n > 0777)
+    error(EXIT_FAILURE, 0, "%s: invalid umask value", str);
+
+  umask(n);
 }
 
 /* Add an argument to the commands that we will call.  Called once for
    every argument. */
-void add_argument(char *newarg)
+static void
+add_argument(const char *newarg)
 {
   if (argcount + 1 >= argsize) {
     argsize = argsize ? argsize * 2 : 4;
     args = realloc(args, argsize * (sizeof(char *)));
-    if (!args) {
-      error("failed to reallocate memory for arguments: %s", strerror(errno));
-      exit(1);
-    }
+    if (!args)
+      error(EXIT_FAILURE, errno, "failed to reallocate memory for arguments");
   }
   args[argcount++] = newarg;
-  args[argcount] = 0;
+  args[argcount] = NULL;
+}
+
+static void __attribute__((noreturn))
+reg_fatal(const char *where, int rc, regex_t *preg)
+{
+  size_t len = regerror(rc, preg, NULL, 0UL);
+  char *errbuf = alloca(len);
+
+  regerror(rc, preg, errbuf, len);
+  error(EXIT_FAILURE, 0, "%s failed: %s", where, errbuf);
+  exit(EXIT_FAILURE);
+}
+
+static regex_t hierre, tradre, excsre, classicalre;
+
+static void
+reg_compile(regex_t *preg, const char *regex)
+{
+  int rc = regcomp(preg, regex, REG_EXTENDED | REG_NOSUB);
+
+  if (rc)
+    reg_fatal("regcomp", rc, preg);
+}
+
+static void
+reg_init(void)
+{
+  static int initialized = 0;
+
+  if (initialized)
+    return;
+  initialized = 1;
+
+  if (lsbsysinit_mode) {
+    reg_compile(&hierre, "^_?([a-z0-9_.]+-)+[a-z0-9]+$");
+    reg_compile(&excsre, "^[a-z0-9-].*rpm(old|new|orig)$");
+    reg_compile(&tradre, "^[a-z0-9][a-z0-9-]*$");
+  }
+  else
+    reg_compile(&classicalre, "^[a-zA-Z0-9_-]+$");
+}
+
+static int
+reg_match(regex_t *preg, const char *sample)
+{
+  int rc = regexec(preg, sample, 0UL, NULL, 0);
+
+  if (rc == 0 || rc == REG_NOMATCH)
+    return rc;
+
+  reg_fatal("regexec", rc, preg);
 }
 
 /* True or false? Is this a valid filename? */
-int valid_name(const struct dirent *d)
+static int
+valid_name(const struct dirent *d)
 {
-  char *c = d->d_name;
-  regex_t hierre, tradre, excsre, classicalre;
+  const char *c = d->d_name;
 
-  /* The regcomps should be moved to program init */
+  reg_init();
 
   if (lsbsysinit_mode) {
+    if (!reg_match(&hierre, c))
+      return reg_match(&excsre, c);
 
-    if (regcomp
-	(&hierre, "^_?([a-z0-9_.]+-)+[a-z0-9]+$", REG_EXTENDED | REG_NOSUB)
-	|| regcomp(&excsre, "^[a-z0-9-].*dpkg-(old|dist|new|tmp)$",
-		   REG_EXTENDED | REG_NOSUB)
-	|| regcomp(&tradre, "^[a-z0-9][a-z0-9-]*$", REG_NOSUB)) {
-      error("regex failure");
-      exit(1);
-    }
-
-    if (!regexec(&hierre, c, 0, NULL, 0))
-      return regexec(&excsre, c, 0, NULL, 0);
-
-    return !regexec(&tradre, c, 0, NULL, 0);
+    return !reg_match(&tradre, c);
   }
-  else {
-    if (regcomp(&classicalre, "^[a-zA-Z0-9_-]+$", REG_EXTENDED | REG_NOSUB)) {
-      error("regex failure");
-      exit(1);
-    }
-    return !regexec(&classicalre, c, 0, NULL, 0);
-  }
+  else
+    return !reg_match(&classicalre, c);
+}
 
+static ssize_t
+write_loop(int fd, const char *buffer, size_t count)
+{
+  ssize_t offset = 0;
+
+  while (count > 0) {
+    ssize_t block = write(fd, &buffer[offset], count);
+
+    if (block < 0 && errno == EINTR)
+      continue;
+    if (block <= 0)
+      return offset ? offset : block;
+    offset += block;
+    count -= block;
+  }
+  return offset;
 }
 
 /* Execute a file */
-void run_part(char *progname)
+static void
+run_part(const char *progname)
 {
   int result;
   int pid;
   int pout[2], perr[2];
 
-  if (report_mode && (pipe(pout) || pipe(perr))) {
-    error("pipe: %s", strerror(errno));
-    exit(1);
-  }
-  if ((pid = fork()) < 0) {
-    error("failed to fork: %s", strerror(errno));
-    exit(1);
-  }
+  if (report_mode && (pipe(pout) || pipe(perr)))
+    error(EXIT_FAILURE, errno, "pipe");
+  if ((pid = fork()) < 0)
+    error(EXIT_FAILURE, errno, "failed to fork");
   else if (!pid) {
     setsid();
     if (report_mode) {
-      if (dup2(pout[1], STDOUT_FILENO) == -1 ||
-	  dup2(perr[1], STDERR_FILENO) == -1) {
-	error("dup2: %s", strerror(errno));
-	exit(1);
-      }
+      if (dup2(pout[1], STDOUT_FILENO) != STDOUT_FILENO ||
+	  dup2(perr[1], STDERR_FILENO) != STDERR_FILENO)
+	error(EXIT_FAILURE, errno, "dup2");
       close(pout[0]);
       close(perr[0]);
       close(pout[1]);
       close(perr[1]);
     }
     args[0] = progname;
-    execv(progname, args);
-    error("failed to exec %s: %s", progname, strerror(errno));
-    exit(1);
+    execv(progname, (char *const *) args);
+    error(EXIT_SUCCESS, errno, "failed to exec %s", progname);
+    _exit(EXIT_FAILURE);
   }
 
   if (report_mode) {
@@ -208,14 +266,11 @@ void run_part(char *progname)
 	  FD_SET(pout[0], &set);
 	if (perr[0] >= 0)
 	  FD_SET(perr[0], &set);
-	r = select(max, &set, 0, 0, 0);
+	r = select(max, &set, NULL, NULL, NULL);
       } while (r < 0 && errno == EINTR);
 
-      if (r < 0) {
-	/* assert(errno != EINTR) */
-	error("select: %s", strerror(errno));
-	exit(1);
-      }
+      if (r < 0)
+	error(EXIT_FAILURE, errno, "select");
       else if (r > 0) {
 	if (pout[0] >= 0 && FD_ISSET(pout[0], &set)) {
 	  c = read(pout[0], buf, sizeof(buf));
@@ -225,7 +280,7 @@ void run_part(char *progname)
 	      fflush(stdout);
 	      printflag = 1;
 	    }
-	    write(STDOUT_FILENO, buf, c);
+	    write_loop(STDOUT_FILENO, buf, (size_t)c);
 	  }
 	  else if (c == 0) {
 	    close(pout[0]);
@@ -234,7 +289,7 @@ void run_part(char *progname)
 	  else if (c < 0) {
 	    close(pout[0]);
 	    pout[0] = -1;
-	    error("failed to read from stdout pipe: %s", strerror (errno)); 
+	    error(EXIT_SUCCESS, errno, "failed to read from stdout pipe"); 
 	  }
 	}
 	if (perr[0] >= 0 && FD_ISSET(perr[0], &set)) {
@@ -245,7 +300,7 @@ void run_part(char *progname)
 	      fflush(stderr);
 	      printflag = 1;
 	    }
-	    write(STDERR_FILENO, buf, c);
+	    write_loop(STDERR_FILENO, buf, (size_t)c);
 	  }
 	  else if (c == 0) {
 	    close(perr[0]);
@@ -254,7 +309,7 @@ void run_part(char *progname)
 	  else if (c < 0) {
 	    close(perr[0]);
 	    perr[0] = -1;
-	    error("failed to read from error pipe: %s", strerror (errno)); 
+	    error(EXIT_SUCCESS, errno, "failed to read from error pipe"); 
 	  }
 	}
       }
@@ -268,117 +323,105 @@ void run_part(char *progname)
   waitpid(pid, &result, 0);
 
   if (WIFEXITED(result) && WEXITSTATUS(result)) {
-    error("%s exited with return code %d", progname, WEXITSTATUS(result));
+    error(EXIT_SUCCESS, 0, "%s exited with return code %d",
+      progname, WEXITSTATUS(result));
     exitstatus = 1;
   }
   else if (WIFSIGNALED(result)) {
-    error("%s exited because of uncaught signal %d", progname,
-	  WTERMSIG(result));
+    error(EXIT_SUCCESS, 0, "%s terminated by signal %d",
+      progname, WTERMSIG(result));
     exitstatus = 1;
   }
 }
 
-
 /* Find the parts to run & call run_part() */
-void run_parts(char *dirname)
+static void
+run_parts(const char *dirname)
 {
   struct dirent **namelist;
   char *filename;
   size_t filename_length, dirname_length;
-  int entries, i, result;
+  int entries, i;
   struct stat st;
 
   /* dirname + "/" */
   dirname_length = strlen(dirname) + 1;
   /* dirname + "/" + ".." + "\0" (This will save one realloc.) */
   filename_length = dirname_length + 2 + 1;
-  if (!(filename = malloc(filename_length))) {
-    error("failed to allocate memory for path: %s", strerror(errno));
-    exit(1);
-  }
+  if (!(filename = malloc(filename_length)))
+    error(EXIT_FAILURE, errno, "failed to allocate memory for path");
   strcpy(filename, dirname);
   strcat(filename, "/");
 
   /* scandir() isn't POSIX, but it makes things easy. */
   entries = scandir(dirname, &namelist, valid_name, alphasort);
-  if (entries < 0) {
-    error("failed to open directory %s: %s", dirname, strerror(errno));
-    exit(1);
-  }
+  if (entries < 0)
+    error(EXIT_FAILURE, errno, "failed to open directory: %s", dirname);
 
   i = reverse_mode ? 0 : entries;
   for (i = reverse_mode ? (entries - 1) : 0;
        reverse_mode ? (i >= 0) : (i < entries); reverse_mode ? i-- : i++) {
     if (filename_length < dirname_length + strlen(namelist[i]->d_name) + 1) {
       filename_length = dirname_length + strlen(namelist[i]->d_name) + 1;
-      if (!(filename = realloc(filename, filename_length))) {
-	error("failed to reallocate memory for path: %s", strerror(errno));
-	exit(1);
-      }
+      if (!(filename = realloc(filename, filename_length)))
+	error(EXIT_FAILURE, errno, "failed to reallocate memory for path");
     }
     strcpy(filename + dirname_length, namelist[i]->d_name);
 
-    strcpy(filename, dirname);
-    strcat(filename, "/");
-    strcat(filename, namelist[i]->d_name);
-
-    result = lstat(filename, &st);
-    if (result < 0) {
-      if (exit_on_error_mode) {
-        error("failed to lstat component %s: %s", filename, strerror(errno));
-        exit(1);
-      }
-      else
-	return;
+    if (stat(filename, &st)) {
+      if (!list_mode)
+	error(EXIT_SUCCESS, errno, "stat: %s", filename);
     }
-
-    if (S_ISREG(st.st_mode) || S_ISLNK(st.st_mode)) {
-      if (!access(filename, X_OK)) {
-	if (test_mode) {
+    else if (S_ISREG(st.st_mode)) {
+      if (list_mode)
+	printf("%s\n", filename);
+      else if (!access(filename, X_OK)) {
+	if (test_mode)
 	  printf("%s\n", filename);
-	}
-	else if (list_mode) {
-	  if (!access(filename, R_OK))
-	    printf("%s\n", filename);
-	}
 	else {
 	  if (verbose_mode)
-	    fprintf(stderr, "run-parts: executing %s\n", filename);
+	    error(EXIT_SUCCESS, 0, "executing %s", filename);
 	  run_part(filename);
-	  if (exitstatus != 0 && exit_on_error_mode) return;
-	}
-      }
-      else if (!access(filename, R_OK)) {
-	if (list_mode) {
-	  printf("%s\n", filename);
-	}
-      }
-      else if (S_ISLNK(st.st_mode)) {
-	if (!list_mode) {
-	  error("run-parts: component %s is a broken symbolic link\n",filename);
-	  exitstatus = 1;
+	  if (exitstatus && exit_on_error_mode) break;
 	}
       }
     }
-    else if (!S_ISDIR(st.st_mode)) {
-      if (!list_mode) {
-	error("run-parts: component %s is not an executable plain file\n",
-	       filename);
-	exitstatus = 1;
-      }
-    }
+    else if (!list_mode && !S_ISDIR(st.st_mode))
+	error(EXIT_SUCCESS, 0, "%s: not a regular file", filename);
 
     free(namelist[i]);
   }
+  for (; reverse_mode ? (i >= 0) : (i < entries); reverse_mode ? i-- : i++)
+    free(namelist[i]);
   free(namelist);
   free(filename);
+}
+
+static void
+sanitize_std_fds(void)
+{
+  int fd;
+
+  for (fd = STDIN_FILENO; fd <= STDERR_FILENO; ++fd) {
+    int null;
+    struct stat st;
+
+    if (fstat(fd, &st) < 0) {
+      null = open("/dev/null", O_RDWR);
+      if (null < 0)
+	  error(EXIT_FAILURE, errno, "open: %s", "/dev/null");
+      if (null > STDERR_FILENO)
+	close(null);
+    }
+  }
 }
 
 /* Process options */
 int main(int argc, char *argv[])
 {
-  umask(022);
-  add_argument(0);
+  error_print_progname = my_error_print_progname;
+  umask(077);
+  add_argument(NULL);
 
   for (;;) {
     int c;
@@ -396,7 +439,7 @@ int main(int argc, char *argv[])
       {"version", 0, 0, 'V'},
       {"lsbsysinit", 0, &lsbsysinit_mode, 1},
       {"exit-on-error", 0, &exit_on_error_mode, 1},
-      {0, 0, 0, 0}
+      {NULL, 0, NULL, 0}
     };
 
     c = getopt_long(argc, argv, "u:ha:vV", long_options, &option_index);
@@ -406,38 +449,34 @@ int main(int argc, char *argv[])
     case 0:
       break;
     case 'u':
-      set_umask();
+      set_umask(optarg);
       break;
     case 'a':
       add_argument(optarg);
       break;
     case 'h':
-      usage();
+      print_help();
       break;
     case 'v':
       verbose_mode = 1;
       break;
     case 'V':
-      version();
+      print_version();
       break;
     default:
-      fprintf(stderr, "Try `run-parts --help' for more information.\n");
-      exit(1);
+      show_usage(NULL);
     }
   }
 
   /* We require exactly one argument: the directory name */
-  if (optind != (argc - 1)) {
-    error("missing operand");
-    fprintf(stderr, "Try `run-parts --help' for more information.\n");
-    exit(1);
-  }
+  if (optind != (argc - 1))
+    show_usage("missing operand");
 
-  if (list_mode && test_mode) {
-    error("--list and --test can not be used together");
-    fprintf(stderr, "Try `run-parts --help' for more information.\n");
-    exit(1);
-  }
+  if (list_mode && test_mode)
+    show_usage("--list and --test can not be used together");
+
+  if (report_mode)
+    sanitize_std_fds();
 
   run_parts(argv[optind]);
 
