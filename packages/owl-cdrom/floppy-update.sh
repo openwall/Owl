66e6f95af44273e11cd6f68650d091489354a918
Solar Designer <solar@openwall.com>
2009-11-20 15:52:47 +0300

    Set the number of inodes to 16, remove lost+found.
    CVS-ID: packages/owl-cdrom/floppy-update.sh 1.7

--- packages/owl-cdrom/floppy-update.sh
+++ packages/owl-cdrom/floppy-update.sh
@@ -1,14 +1,15 @@
 #!/bin/sh
-# $Owl: Owl/packages/owl-cdrom/Attic/floppy-update.sh,v 1.6 2008/07/05 20:17:42 solar Exp $
+# $Owl: Owl/packages/owl-cdrom/Attic/floppy-update.sh,v 1.7 2009/11/20 12:52:47 solar Exp $
 
 set -e
 
 dd if=/dev/zero of=floppy.image bs=36k count=80
-/sbin/mke2fs -m 0 -F floppy.image
+/sbin/mke2fs -m 0 -N 16 -F floppy.image
 
 mount floppy.image /mnt/floppy -oloop
 trap 'umount /mnt/floppy' EXIT HUP INT TERM
 
+rmdir /mnt/floppy/lost+found
 mkdir -m 700 /mnt/floppy/boot
 if [ -e boot.b ]; then
 	cp -p boot.b /mnt/floppy/boot/
