30445114ceeed559657711087661704294855bb1
Solar Designer <solar@openwall.com>
2009-11-22 01:16:25 +0300

    Disable the "regular checks" (expected fsck runs) for the tiny ext2 filesystem on the "floppy".
    CVS-ID: packages/owl-cdrom/floppy-update.sh 1.8

--- packages/owl-cdrom/floppy-update.sh
+++ packages/owl-cdrom/floppy-update.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
-# $Owl: Owl/packages/owl-cdrom/Attic/floppy-update.sh,v 1.7 2009/11/20 12:52:47 solar Exp $
+# $Owl: Owl/packages/owl-cdrom/Attic/floppy-update.sh,v 1.8 2009/11/21 22:16:25 solar Exp $
 
 set -e
 
 dd if=/dev/zero of=floppy.image bs=36k count=80
 /sbin/mke2fs -m 0 -N 16 -F floppy.image
+/sbin/tune2fs -c 0 -i 0 floppy.image
 
 mount floppy.image /mnt/floppy -oloop
 trap 'umount /mnt/floppy' EXIT HUP INT TERM
